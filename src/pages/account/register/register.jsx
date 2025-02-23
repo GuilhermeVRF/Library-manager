6bea7a71ae1593213bc2602c7139b5b4e1844824
EdOc-PS <eeuardooctavio@gmail.com>
2025-02-23 16:10:17 -0300

    feat: fixing the regsiter pages

--- src/pages/account/register/register.jsx
+++ src/pages/account/register/register.jsx
@@ -1,15 +1,30 @@
-import Input from "../../../components/input/input"
+import Input from "../../../components/input/input";
+import style from './register.module.css';
 
 function Register() {
-
   return (
-    <>
-      <form action="">
-        <Input/>
+    <div className={style.container_register}>
+      <h1 className={style.title}>Registre o usuário</h1>
+      <form className={style.form} action="">
+        <div className="input_group">
+          <label htmlFor="name" className="label">Nome:</label>
+          <Input type="text" placeholder="Nome" />
+        </div>
         
+        <div className="input_group">
+          <label htmlFor="email" className="label">Email:</label>
+          <Input type="text" placeholder="Email" />
+        </div>
+        
+        <div className="input_group">
+          <label htmlFor="password" className="label">Senha:</label>
+          <Input type="password" placeholder="Senha"/>
+        </div>
+        
+        <button type="submit" className="btn btn_success">Registrar</button>
       </form>
-    </>
-  )
+    </div>
+  );
 }
 
-export default Register
+export default Register;
