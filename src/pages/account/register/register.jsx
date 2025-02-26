858fd4ae6ce5df1e04fb4af86da94dd05559557b
GuilhermeVRF <guilhermevr.figueiredo@outlook.com>
2025-02-26 08:03:26 -0300

    feat: implement user registration form with API integration

--- src/pages/account/register/register.jsx
+++ src/pages/account/register/register.jsx
@@ -1,25 +1,66 @@
+import { useState } from "react";
 import Input from "../../../components/input/input";
 import style from './register.module.css';
 import { Link } from 'react-router-dom';
+import { useNavigate } from 'react-router-dom';
 
 function Register() {
+  const [name, setName] = useState('');
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const navigate = useNavigate();
+  
+  const handleRegister = async (e) => {
+    e.preventDefault(); 
+
+    // Objeto com os dados do formulário
+    const userData = {
+      name: name,
+      email: email,
+      password: password,
+    };
+
+    try {
+      // Fazendo o fetch para a API
+      const response = await fetch("http://localhost:8080/library/person", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify(userData), 
+      });
+
+      if (!response.ok) {
+        throw new Error("Erro ao cadastrar usuário");
+      }
+
+      const data = await response.json();
+      console.log("Usuário cadastrado com sucesso:", data);
+
+      navigate('/login');
+    } catch (error) {
+      console.error("Erro:", error);
+      alert("Erro ao cadastrar usuário. Tente novamente.");
+    }
+  }
+
   return (
     <div className={style.container_register}>
       <h1 className={style.title}>Registre o usuário</h1>
-      <form className={style.form} action="">
+      <form className={style.form} onSubmit={handleRegister}>
         <div className="input_group">
           <label htmlFor="name" className="label">Nome:</label>
-          <Input type="text" placeholder="Nome" />
+          <Input type="text" placeholder="Nome" onChange={(e) => setName(e.target.value)}/>
         </div>
         
         <div className="input_group">
           <label htmlFor="email" className="label">Email:</label>
-          <Input type="text" placeholder="Email" />
+          <Input type="text" placeholder="Email" onChange={(e) => setEmail(e.target.value)}/>
         </div>
         
         <div className="input_group">
           <label htmlFor="password" className="label">Senha:</label>
-          <Input type="password" placeholder="Senha"/>
+          <Input type="password" placeholder="Senha" onChange={(e) => setPassword(e.target.value)}/>
         </div>
         
         <button type="submit" className="btn btn_success">Registrar</button>
