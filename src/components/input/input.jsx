3adcf4cf865970645d39752eb9ed3082f20dfd6f
GuilhermeVRF <guilhermevr.figueiredo@outlook.com>
2025-02-26 07:43:22 -0300

    feat: add onChange prop to Input component

--- src/components/input/input.jsx
+++ src/components/input/input.jsx
@@ -1,11 +1,11 @@
 import style from "./input.module.css";
 
-function Input(label, type, placeholder) {
+function Input({label, type, placeholder, onChange}) {
   return (
     <>
   
         <div className={style.input_body}>
-          <input type={type} placeholder={placeholder} id={label} required />
+          <input type={type} placeholder={placeholder} id={label} required onChange={onChange}/>
         </div>
     </>
   );
